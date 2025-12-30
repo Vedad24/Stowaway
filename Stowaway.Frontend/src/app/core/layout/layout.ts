bad8520cd8247f8d79374d7a28bfdf098d52bdc4
vedad <vedolino2004@gmail.com>
2025-12-30 18:25:03 +0100

    header css updates

--- Stowaway.Frontend/src/app/core/layout/layout.ts
+++ Stowaway.Frontend/src/app/core/layout/layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from './navbar/navbar';
+import { Sidebar } from './sidebar/sidebar';
+import { MainSection } from './main-section/main-section';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [Navbar,Sidebar,MainSection],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
