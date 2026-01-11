a561217956dc3b8577fb36540104eb21bdcd6d06
vedad <vedolino2004@gmail.com>
2026-01-11 15:19:40 +0100

    merge with amel branch and fix conflicts

--- Stowaway.Frontend/src/app/app.routes.ts
+++ Stowaway.Frontend/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './core/landing-page/landing-page';
+import { LoginPage } from './core/login-page/login-page';
+import { Warehouse } from './core/warehouse/warehouse/warehouse';
 
 export const routes: Routes = [
     {
@@ -8,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginPage
-    }
+    },
+    {
+        path: 'warehouse',
+        component: Warehouse
+    },
 ];
