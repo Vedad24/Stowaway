d49a5d85a1215361ba5f656bf1ef80695986f6e7
Amel <98781312+amel-fit@users.noreply.github.com>
2026-06-28 19:08:03 +0200

    feat login and signup pages tweaked
    login page: invalidd credentials snackbar update

--- Stowaway.Frontend/src/app/core/identity/sign-up/sign-up.ts
+++ Stowaway.Frontend/src/app/core/identity/sign-up/sign-up.ts
@@ -1,16 +1,17 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserService } from '../../../services/identity/user/user-service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CreateUserCommand, RoleName } from '../../../services/identity/user/user-service.models';
-import { MatInputModule } from "@angular/material/input";
-import {MatStepperModule} from "@angular/material/stepper"
+import { CreateUserCommand } from '../../../services/identity/user/user-service.models';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { catchError, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon'
+
 @Component({
   selector: 'app-sign-up',
-  imports: [MatInputModule, MatStepperModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatInputModule, MatStepperModule, ReactiveFormsModule, MatButtonModule, MatCardModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
@@ -19,39 +20,42 @@ export class SignUp {
   userService = inject(UserService);
   fb = inject(FormBuilder);
 
-   emailForm: FormGroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
-    });
-    profileForm: FormGroup = this.fb.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required]
-      });
-  passwordForm: FormGroup = this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    });
+  emailForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
-  createUser()
-  {
-    const payload : CreateUserCommand = 
-    {
-        email: this.emailForm.value.email!,
-        firstName: this.profileForm.value.firstName!,
-        lastName: this.profileForm.value.lastName!,
-        password: this.passwordForm.value.password!,
-    }
-    //console.log(payload);
+  profileForm: FormGroup = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+  });
+
+  passwordForm: FormGroup = this.fb.group({
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  createUser() {
+    const payload: CreateUserCommand = {
+      email: this.emailForm.value.email!,
+      firstName: this.profileForm.value.firstName!,
+      lastName: this.profileForm.value.lastName!,
+      password: this.passwordForm.value.password!,
+    };
+
     this.userService.create(payload)
-    .pipe(
-      tap((response) => 
-      {
-        console.log("Created user with ID", response);
-        this.userService.setData({email: payload.email, password: payload.password});
-        this.router.navigate(['/login']);
-      }),
-      catchError( (err) => {
-        throw new Error(err);
-      })
-    )
-    .subscribe();
+      .pipe(
+        tap((response) => {
+          console.log('Created user with ID', response);
+          this.userService.setData({ email: payload.email, password: payload.password });
+          this.router.navigate(['/login']);
+        }),
+        catchError((err) => {
+          throw new Error(err);
+        })
+      )
+      .subscribe();
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
   }
 }
--- Stowaway.Frontend/src/app/core/login-page/login-page.ts
+++ Stowaway.Frontend/src/app/core/login-page/login-page.ts
@@ -1,72 +1,84 @@
 import { Component, inject } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
-import {MatInputModule} from '@angular/material/input'
-import { MatAnchor, MatButtonModule } from "@angular/material/button";
-import { AuthService } from '../../services/identity/auth/auth-service'; 
-import { CurrentUserService } from '../../services/identity/auth/current-user-service'; 
-import { catchError, tap } from 'rxjs';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { AuthService } from '../../services/identity/auth/auth-service';
+import { CurrentUserService } from '../../services/identity/auth/current-user-service';
+import { catchError, of, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { fileURLToPath } from 'url';
-import { UserService } from '../../services/identity/user/user-service'; 
-import { MatIcon } from "@angular/material/icon";
+import { UserService } from '../../services/identity/user/user-service';
+
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule, MatInputModule, MatAnchor, MatButtonModule, MatIcon],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatCardModule, MatIconModule, MatSnackBarModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
-  
   authService = inject(AuthService);
   currentUserService = inject(CurrentUserService);
   router = inject(Router);
   userService = inject(UserService);
+  snackBar = inject(MatSnackBar);
 
   hidePassword = true;
-  ngOnInit()
-  {
+
+  ngOnInit() {
     const data = this.userService.signUpData;
-    //console.log("Data:", data);
-    if(data.email != null && data.password != null)
-    {
-      console.log("Gotten data", data);
-      this.loginForm.patchValue(
-        {
-          email: data.email,
-          password : data.password
-        }
-      )
+    if (data.email != null && data.password != null) {
+      this.loginForm.patchValue({
+        email: data.email,
+        password: data.password,
+      });
       this.userService.clearData();
     }
   }
+
   tryLogin() {
+    if (!this.loginForm.valid) {
+      return;
+    }
 
-    try{
-
-      this.authService.login(this.loginForm.value.email!, this.loginForm.value.password!)
+    this.authService.login(this.loginForm.value.email!, this.loginForm.value.password!)
       .pipe(
-        
-        catchError( err => { 
-          throw new Error('Login request failed: ' + err.message);
-        }),
-
         tap((success) => {
-          if(success){
-            this.router.navigate(['main']); 
+          if (success) {
+            this.router.navigate(['main']);
+            return;
           }
+
+          this.showInvalidCredentials();
+        }),
+        catchError(() => {
+          this.showInvalidCredentials();
+          return of(false);
         })
-      ).subscribe();
-    }
-    catch(error){
-      console.error(error);
-    }
+      )
+      .subscribe();
   }
-  
-  loginForm = new FormGroup(
-    {
-      email : new FormControl('admin@market.local',[Validators.email, Validators.required]),
-      password : new FormControl('Admin123!', Validators.required)
-    }
-  )
-  
+
+  showInvalidCredentials(): void {
+    this.snackBar.open('Invalid Credentials', 'Dismiss', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['invalid-credentials-snackbar'],
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  goToSignUp(): void {
+    this.router.navigate(['/sign-up']);
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl('admin@market.local', [Validators.email, Validators.required]),
+    password: new FormControl('Admin123!', Validators.required),
+  });
 }
