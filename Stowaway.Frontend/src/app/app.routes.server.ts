a217c19836a404145ef45bf248f4eec15d039332
Amel <98781312+amel-fit@users.noreply.github.com>
2026-06-28 18:58:09 +0200

    feat landing page touch up
    No live data injection yet

--- Stowaway.Frontend/src/app/app.routes.server.ts
+++ Stowaway.Frontend/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'warehouse/edit/:abc',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'item/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'supplier/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- Stowaway.Frontend/src/app/core/landing-page/landing-page.ts
+++ Stowaway.Frontend/src/app/core/landing-page/landing-page.ts
@@ -1,20 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
-import { LoginPage } from '../login-page/login-page'; 
-import {MatButtonModule} from '@angular/material/button'
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-landing-page',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatCardModule, MatToolbarModule],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
 export class LandingPage {
-  goToSignUp() {
-    this.router.navigate(['/sign-up'])
+  readonly router = inject(Router);
+
+  goToSignUp(): void {
+    this.router.navigate(['/sign-up']);
   }
-  router = inject(Router)
-  goToLogin()
-  {
+
+  goToLogin(): void {
     this.router.navigate(['/login']);
   }
 }
