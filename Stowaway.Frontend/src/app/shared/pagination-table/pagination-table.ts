84fa0f0680901f4799457ced64729e31df4913b4
Amel <98781312+amel-fit@users.noreply.github.com>
2026-08-05 03:11:48 +0200

    feat: add buttons to pagination table

--- Stowaway.Frontend/src/app/shared/pagination-table/pagination-table.ts
+++ Stowaway.Frontend/src/app/shared/pagination-table/pagination-table.ts
@@ -1,10 +1,11 @@
 import { Component, effect, inject, Inject, Injectable, Input, signal, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-pagination-table',
-  imports: [ MatPaginator, MatTableModule],
+  imports: [ MatPaginator, MatTableModule, MatIconModule],
   templateUrl: './pagination-table.html',
   styleUrl: './pagination-table.css',
 })
@@ -38,11 +39,20 @@ export class PaginationTable<TDto>{
   }
 
   getCellValue(row: TDto, column: TableColumnDef<TDto>): string {
-    return column.cell(row);
+    return column.cell?.(row) ?? '';
   }
 }
 export interface TableColumnDef<TDto>{
   columnDef: string,
   header: string,
-  cell: (row: TDto) => string,
+  type?: 'text' | 'action'; // defaults to 'text'
+  cell?: (row: TDto) => string; // Used if type is 'text'
+  buttons?: {
+    type: 'icon' | 'text'; //uses matIcon if type is icon (default)
+    label?: string; //does nothing if type is icon
+    icon?: string; //does nothing if type is text
+    color?: string;
+    action: (row: TDto) => void;
+  }[]; // Used if type is 'action'
+
 }
