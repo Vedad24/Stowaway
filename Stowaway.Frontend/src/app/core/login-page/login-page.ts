a785e9b9f7b8c0374cbd9c4afc31d3fdae91b87a
vedad <vedolino2004@gmail.com>
2026-09-05 00:18:10 +0200

    patch: add lazy loading and make modules

--- Stowaway.Frontend/src/app/core/login-page/login-page.ts
+++ Stowaway.Frontend/src/app/core/login-page/login-page.ts
@@ -1,10 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from '../../services/identity/auth/auth-service';
 import { CurrentUserService } from '../../services/identity/auth/current-user-service';
 import { catchError, of, tap } from 'rxjs';
@@ -13,7 +9,7 @@ import { UserService } from '../../services/identity/user/user-service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatCardModule, MatIconModule, MatSnackBarModule],
+  standalone: false,
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
